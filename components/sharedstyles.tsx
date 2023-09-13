8960da5184e1ccc74109920c6caae81a54da7993
jeremyben-meir <jsb459@cornell.edu>
2023-09-13 08:44:03 -0400

    eslint

--- components/sharedstyles.tsx
+++ components/sharedstyles.tsx
@@ -1,22 +1,32 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Container = styled.div`
-  padding: 0 0.5rem;
   display: flex;
   flex-flow: column nowrap;
   justify-content: center;
   align-items: center;
   height: 100vh;
   min-height: 100vh;
-`
+`;
 const Main = styled.main`
   padding: 5rem 0;
   flex: 1;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  // border: 1px solid red;
+  width: 500px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  height: 100px;
+  box-shadow: 0px 0px 10px #0003;
+  width: 100%;
+  border-radius: 12px;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Title = styled.h1`
   margin: 0;
@@ -34,13 +44,13 @@ const Title = styled.h1`
       text-decoration: underline;
     }
   }
-`
+`;
 
 const Description = styled.p`
   text-align: center;
   line-height: 1.5;
   font-size: 1.5rem;
-`
+`;
 const CodeTag = styled.code`
   background: #fafafa;
   border-radius: 5px;
@@ -49,6 +59,6 @@ const CodeTag = styled.code`
   font-size: 1.1rem;
   font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
     Bitstream Vera Sans Mono, Courier New, monospace;
-`
+`;
 
-export { Container, Main, Title, Description, CodeTag }
+export { Container, Main, Title, Description, CodeTag, Header };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
+import Head from "next/head";
 import {
   Container,
   Main,
   Title,
   Description,
   CodeTag,
-} from '../components/sharedstyles'
-import Cards from '../components/cards'
+  Header,
+} from "../components/sharedstyles";
 
 export default function Home() {
   return (
@@ -17,17 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main>
-        <Title>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </Title>
-
-        <Description>
-          Get started by editing
-          <CodeTag>pages/index.tsx</CodeTag>
-        </Description>
-
-        <Cards />
+        <Header>dawd</Header>
       </Main>
     </Container>
-  )
+  );
 }
